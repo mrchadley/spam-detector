65f43b6266bea14dfbd653127118fd87d4b0d963
mrchadley <marc.tremblay@uoit.net>
2016-03-10 12:40:11 -0500

    iterates through the files and adds words to a map for each file if it shows up in that file - marc

--- src/SpamDetector/Main.java
+++ src/SpamDetector/Main.java
@@ -8,12 +8,17 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Main extends Application
 {
     DirectoryChooser directoryChooser = new DirectoryChooser();
-    File trainingDirectory;
-    File testingDirectory;
+
+    File dataDirectory;
+
+    Map<String, Integer> hamMap = new TreeMap<>();
+    Map<String, Integer> spamMap = new TreeMap<>();
 
 
     @Override
@@ -23,26 +28,61 @@ public class Main extends Application
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
 
-        //choose training directory
-        directoryChooser.setTitle("Choose Training Directory");
+        //choose data directory
+        directoryChooser.setTitle("Choose data Directory");
         directoryChooser.setInitialDirectory(new File("."));
-        trainingDirectory = directoryChooser.showDialog(primaryStage);
+        dataDirectory = directoryChooser.showDialog(primaryStage);
+
+
 
         //train here
-        System.out.println("Training Dir: " + trainingDirectory); //log
+        train();
 
 
-        //choose testing directory
-        directoryChooser.setTitle("Choose Testing Directory");
-        directoryChooser.setInitialDirectory(new File("."));
-        testingDirectory = directoryChooser.showDialog(primaryStage);
+
+
 
         //test here
-        System.out.println("Testing dir: " + testingDirectory); //log
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void train() throws IOException {
+        CountWords(new File(dataDirectory + "/train/ham"), hamMap);
+        CountWords(new File(dataDirectory + "/train/spam"), spamMap);
+
+        System.out.println(hamMap);
+        System.out.println(spamMap);
+    }
+    void CountWords(File dir, Map<String, Integer> map) throws IOException
+    {
+        try {
+
+
+            for (File file : dir.listFiles()) {
+                LinkedList<String> words = new LinkedList<>(); //holds a string if it shows up in the file
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNext()) {
+                    String word = scanner.next();
+                    if (!words.contains(word)) {
+                        words.add(word);
+                    }
+                }
+                //adds strings from words list to the map so that it only adds increases
+                //the count if it shows up in the file, not for each time it shows in a file
+                for (String string : words) {
+                    if (map.containsKey(string)) {
+                        map.put(string, map.get(string) + 1);
+                    } else {
+                        map.put(string, 1);
+                    }
+                }
+            }
+        }catch(IOException e){
+        }
     }
 }
