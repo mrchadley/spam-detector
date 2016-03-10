e15809b54d08c0facfc08c75461eb03fc8bb0ac9
mrchadley <marc.tremblay@uoit.net>
2016-03-10 10:18:49 -0500

    slightly formatted the directory choosing stuffz - marc

--- src/SpamDetector/Main.java
+++ src/SpamDetector/Main.java
@@ -9,22 +9,36 @@ import javafx.stage.Stage;
 
 import java.io.File;
 
-public class Main extends Application {
-
-    File mainDirectory;
+public class Main extends Application
+{
+    DirectoryChooser directoryChooser = new DirectoryChooser();
+    File trainingDirectory;
+    File testingDirectory;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Spam Detector");
-
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setInitialDirectory(new File("."));
-        mainDirectory = directoryChooser.showDialog(primaryStage);
-
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
+
+        //choose training directory
+        directoryChooser.setTitle("Choose Training Directory");
+        directoryChooser.setInitialDirectory(new File("."));
+        trainingDirectory = directoryChooser.showDialog(primaryStage);
+
+        //train here
+        System.out.println("Training Dir: " + trainingDirectory); //log
+
+
+        //choose testing directory
+        directoryChooser.setTitle("Choose Testing Directory");
+        directoryChooser.setInitialDirectory(new File("."));
+        testingDirectory = directoryChooser.showDialog(primaryStage);
+
+        //test here
+        System.out.println("Testing dir: " + testingDirectory); //log
     }
 
 
